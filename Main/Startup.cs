f0aaffec264e26b8f4fcf20daaac30a7b41cd07a
Bridget Cashel <bec46@drexel.edu>
2018-01-17 21:47:37 -0500

    Changes to solution to get React.AspNet installed

--- Main/Startup.cs
+++ Main/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Main.Data.Interfaces;
 using Main.Data.MockData;
+using Microsoft.AspNetCore.Http;
+using React.AspNet;
 
 namespace Main
 {
@@ -23,7 +25,9 @@ namespace Main
 			services.AddTransient<IDatabase_Repository, MockDatabase_Repository>();
 			services.AddTransient<IDatabaseServerRepository, MockDatabaseServerRepository>();
 			services.AddTransient<IDataLoaderAPIRepository, MockDataLoaderAPIRepository>();
-			services.AddMvc();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +35,26 @@ namespace Main
         {
 			app.UseDeveloperExceptionPage();
 			app.UseStatusCodePages();
-			app.UseStaticFiles();
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+            });
+            app.UseStaticFiles();
 			app.UseMvcWithDefaultRoute();
         }
     }
