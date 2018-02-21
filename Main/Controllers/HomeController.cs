8f4d1bea5ccf231f06bdfa8223993ec1b652d13d
Mahmuda <mkl.drexel@gmail.com>
2018-02-21 04:24:30 -0500

    Registration and Login

--- Main/Controllers/HomeController.cs
+++ Main/Controllers/HomeController.cs
@@ -5,27 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Main.Data;
 using Main.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Main.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
 		private readonly BamsDbContext _dbcontext;
 
 		public HomeController(BamsDbContext dbcontext)
 		{
-            _dbcontext = dbcontext;
+            _dbcontext = dbcontext; 
 		}
 
-        public ViewResult Index()
+        public IActionResult Index()
         {
             var homeViewModel = new HomeViewModel
             {
                 PreferredAssets = _dbcontext.Assets.Where(m => m.isPreferredAsset).ToList()
-			};
-			return View(homeViewModel);
+            };
+            return View(homeViewModel);
         }
+
+        // Use tag [AllowAnonymous] for action that does not require authorization.
     }
 }
