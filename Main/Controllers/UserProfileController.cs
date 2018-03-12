4d1cc50a65673d8e1177b7fbe8c0ce0fd9b8327c
Mahmuda <mkl.drexel@gmail.com>
2018-03-12 12:52:19 -0400

    User Profile screen so far

---
+++ Main/Controllers/UserProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Main.Data;
+using Main.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Main.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private readonly BamsDbContext _dbcontext;
+
+        public UserProfileController(BamsDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Title = "User Profile";
+
+            var user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var userDetails = _dbcontext.Users.Where(x => x.Id == user).FirstOrDefault();
+            string Email = userDetails.UserName;
+            string FirstName = userDetails.FirstName;
+            string LastName = userDetails.LastName;
+
+            UserProfileViewModel vm = new UserProfileViewModel(FirstName, LastName, Email);
+            return View(vm);
+        }
+
+    }
+}
---
+++ Main/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Main.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public UserProfileViewModel(){}
+        public UserProfileViewModel(string firstName, string lastName, string email){
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress, Required]
+        public string Email { get; set; } 
+    }
+}
